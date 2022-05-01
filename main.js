b9f1a279ced55bff6d9f1fb01b826bb10865f0be
patek_cz <52839023+patekcz@users.noreply.github.com>
2022-05-01 03:06:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+//
+// Create by patek_cz 
+// https://dsc.gg/qverlix
+// 
+
+
+
+// Modules
+const Discord = require('discord.js');
+const five = require('johnny-five');
+// Bot permissions
+const Client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+// Usb port
+const board = new five.Board({ port: 'COM3' })
+
+// Bot Token
+Client.login('HERE TOKEN')
+
+
+
+board.on('ready', () => {
+  const led = new five.Led(13); // Arduino port 13 Led.(13);
+  Client.on('message', message => {
+    if (message.content === 'led on') led.off()
+    else if (message.content === 'led off') led.on()
+  });
+});
+
+
+// Bot Status
+Client.on('ready', () => {
+    console.log('Client is Online!')
+    Client.user.setActivity('by: patek_cz', { type: "WATCHING" })
+});
+
+
+
+
+//
+// Create by patek_cz 
+// https://dsc.gg/qverlix
+// 
